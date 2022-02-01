6b6ce99b5bf9ad08ef4325409b25ec5e12b65343
amnahkhatun <amna.khatun1605@gmail.com>
2022-02-01 12:27:51 +0530

    final commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import netflix from '../images/netflix.jpg';
 import avatar from '../images/Netflix-avatar.png'
 import './nav.css'
 
 export default function Nav() {
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setShow(true);
+      } else {
+        setShow(false)
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    }
+  }, [])
   return (
     <div>
-      <div className="nav">
+      <div className={`nav ${show && "nav__black"}`}>
         <img
           className="nav__logo"
           src={netflix}
